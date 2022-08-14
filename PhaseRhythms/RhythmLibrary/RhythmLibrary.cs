ee68e617b492fabcac2bab5f4008e5bad9e4e851
jordan.werre <jordan.r.werre@gmail.com>
2022-08-14 00:04:16 -0400

    begin developing logic

---
+++ PhaseRhythms/RhythmLibrary/RhythmLibrary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace RhythmLibrary
+{
+    public class Rhythm
+    {
+        string originalInput;
+        public Rhythm(string str)
+        {
+            originalInput = str;
+            /*
+            char[] charBeats = str.ToCharArray();
+            int[] beats = new int[charBeats.Length];
+            for (int i = 0; i < charBeats.Length; i++)
+            {
+                beats[i] = int.Parse(charBeats[i].ToString());
+                Console.WriteLine(beats[i]);
+            }
+            */
+        }
+
+        public bool matches(string str, bool checkInitial)
+        {
+            // if applicable, check if input matches original
+            if (checkInitial && originalInput == str)
+            {
+                return true;
+            }
+            // split into into char and make into linked list
+            char[] beats = str.ToCharArray();
+            LinkedList<char> beatList = new LinkedList<char>(beats);
+            for (var i = 0; i < beats.Length - 1; i++)
+            {
+                // Move first item in linked list to the end
+                beatList.AddLast(beatList.First()); 
+                beatList.RemoveFirst();
+                // join into a string
+                string check = String.Join("", beatList);
+                Console.WriteLine(check);
+                // check if string matches original
+                if (originalInput == check)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
